db5213bb9d067b3be64bc175c3f2cf57f6f0f5d3
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-04 18:42:21 -0500

    add initial migrations for db tables

--- database/migrations/2019_02_04_233926_create_groupevents_table.php
+++ database/migrations/2019_02_04_233926_create_groupevents_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGroupeventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('groupevents', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar_url');
-            $table->rememberToken();
+            $table->string('header_url');
+            $table->string('description');
+            $table->string('start_date');
+            $table->string('start_time')->nullable();
+            $table->string('end_date')->nullable();
+            $table->string('end_time')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('groupevents');
     }
 }
