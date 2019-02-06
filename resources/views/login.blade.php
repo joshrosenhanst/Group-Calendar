79bfe5b115e9e22b4a617419016a6832bd9ab2f9
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-06 15:03:17 -0500

    add styles to the login template

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,22 @@
 @section('content')
 <form id="login_form">
   @csrf
-  <h1>This is the login page.</h1>
-  <label>Email<input type="email" placeholder="Email" name="email"></label>
-  <label>Password<input type="password" placeholder="Password" name="password"></label>
+  <header>
+    <h1>Group Calendar Login</h1>
+  </header>
+  <main>
+      <label>Email Address<input type="email" placeholder="Email Address" name="email"></label>
+      <label>Password<input type="password" placeholder="Password" name="password"></label>
+      <div class="split_layout">
+        <label class="checkbox">
+          <input type="checkbox" name="remember"> Remember Me
+        </label>
+        <a href="#">Forgot Your Password?</a>
+      </div>
+  </main>
+  <footer>
+    <button type="submit" class="is-link">Login</button>
+    <a href="#" class="button is-text">Try a Demo</a>
+  </footer>
 </form>
 @endsection
