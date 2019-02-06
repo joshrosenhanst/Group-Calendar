ce39a1911e10c670bf0d1349f505de9f8ded4bc0
Josh mac <joshrh88@gmail.com>
2019-02-05 19:21:29 -0500

    add initial web routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,39 @@
 */
 
 Route::get('/', function () {
-    return view('login');
+  return view('login');
+  //if auth, go to group home page
+  //else go to login page
 });
+
+/* Calendar */
+Route::get('/calendar','EventController@calendar');
+
+/* Events */
+Route::group('events', function(){
+  Route::get('/','EventController@index');
+  Route::get('/new','EventController@new');
+  Route::get('/{event}','EventController@view');
+  Route::get('/{event}/edit','EventController@edit');
+  Route::get('/{event}/delete','EventController@delete');
+  Route::put('/create', 'EventController@create');
+  Route::put('/update', 'EventController@update');
+  Route::put('/delete', 'EventController@destroy');
+});
+
+/* Groups - member controls */
+Route::group('groups', function(){
+  Route::get('/', 'GroupController@index'); // my groups
+  Route::get('/{group}', 'GroupController@view'); // view group
+});
+
+/* Admin controls */
+
+Route::group('admin', function(){
+  Route::group('groups', function(){
+    Route::get('/{group}/edit', 'GroupController@view'); // view group    
+  });
+});
+
+/* User management */
+
