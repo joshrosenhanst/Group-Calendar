26e9923678a001cb6f114df836d7c6c4bc344e3e
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-07 18:08:30 -0500

    redo header css naming conventions

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,30 +1,30 @@
-<header id="site_header" class="navbar">
+<header id="site_header">
   <nav class="navbar">
       <div class="navbar-left">
-        <a href="/" id="site_logo" class="navbar-item">Logo</a>
+        <a href="/" class="navbar_item navbar_logo">Logo</a>
       </div>
       @auth
-      <div class="navbar-right">
-        <a href="/notifications" class="navbar-item">
+      <div class="navbar_right">
+        <a href="/notifications" class="navbar_item">
           {{--icon or count badge--}} Notifications
         </a>
-        <div class="navbar-item dropdown">
-          <a class="navbar-dropdown-toggle">
+        <div class="navbar_item navbar_dropdown">
+          <a class="dropdown_toggle">
             <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }} avatar">
             {{ Auth::user()->name }}
           </a>
-          <div class="dropdown-items">
-            <a href="{{ route('profile.index') }}" class="dropdown-item">My Profile</a>
-            <a href="{{ route('groups.index') }}" class="dropdown-item">My Groups</a>
-            <hr class="dropdown-divider">
-            <a href="{{ route('logout') }}" class="dropdown-item">Logout</a>
+          <div class="dropdown_items">
+            <a href="{{ route('profile.index') }}" class="dropdown_item">My Profile</a>
+            <a href="{{ route('groups.index') }}" class="dropdown_item">My Groups</a>
+            <hr class="dropdown_divider">
+            <a href="{{ route('logout') }}" class="dropdown_item">Logout</a>
           </div>
         </div>
       </div>
       @else
-      <div class="navbar-right">
-        <a href="{{ route('login') }}" class="navbar-item">Login</a>
-        <a href="{{ route('demo') }}" class="navbar-item">Try a Demo</a>
+      <div class="navbar_right">
+        <a href="{{ route('login') }}" class="navbar_item">Login</a>
+        <a href="{{ route('demo') }}" class="navbar_item">Try a Demo</a>
       </div>
       @endif
   </nav>
