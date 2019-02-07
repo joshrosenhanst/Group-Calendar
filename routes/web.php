9bdffd7fdde6e7815a6e05563b81b28b4da4ceb5
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-07 14:51:42 -0500

    add names to all routes

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ Route::middleware('auth')->prefix('events')->name('events.')->group(function(){
   Route::get('/','EventController@index');
   Route::get('/new','EventController@new')->name('new');
   Route::get('/{event}','EventController@view')->name('view');
-  Route::get('/{event}/edit','EventController@edit');
-  Route::get('/{event}/delete','EventController@delete');
-  Route::put('/create', 'EventController@create');
-  Route::put('/update', 'EventController@update');
-  Route::delete('/delete', 'EventController@destroy');
+  Route::get('/{event}/edit','EventController@edit')->name('edit');
+  Route::get('/{event}/delete','EventController@delete')->name('delete');
+  Route::put('/create', 'EventController@create')->name('create');
+  Route::put('/update', 'EventController@update')->name('update');
+  Route::delete('/delete', 'EventController@destroy')->name('destroy');
 });
 
 /* Groups */
@@ -27,27 +27,27 @@ Route::middleware('auth')->prefix('groups')->name('groups.')->group(function(){
   Route::get('/', 'GroupController@index')->name('index'); // my groups
   Route::get('/{group}', 'GroupController@view')->name('view'); // view group
   Route::get('/new','GroupController@new')->name('new');
-  Route::get('/{group}/edit','GroupController@edit');
-  Route::get('/{group}/delete','GroupController@delete');
-  Route::get('/{group}/invite','GroupController@invite');
-  Route::get('/{group}/join','GroupController@join');
-  Route::put('/create', 'GroupController@create');
-  Route::put('/sendInvite', 'GroupController@sendInvite');
-  Route::put('/acceptInvite', 'GroupController@acceptInvite');
-  Route::put('/update', 'GroupController@update');
-  Route::delete('/delete', 'GroupController@destroy');
+  Route::get('/{group}/edit','GroupController@edit')->name('edit');
+  Route::get('/{group}/delete','GroupController@delete')->name('delete');
+  Route::get('/{group}/invite','GroupController@invite')->name('invite');
+  Route::get('/{group}/join','GroupController@join')->name('join');
+  Route::put('/create', 'GroupController@create')->name('create');
+  Route::put('/sendInvite', 'GroupController@sendInvite')->name('sendInvite');
+  Route::put('/acceptInvite', 'GroupController@acceptInvite')->name('acceptInvite');
+  Route::put('/update', 'GroupController@update')->name('update');
+  Route::delete('/delete', 'GroupController@destroy')->name('destroy');
 });
 
 /* My Profile */
 Route::middleware('auth')->prefix('profile')->name('profile.')->group(function(){
-  Route::get('/','ProfileController@index'); // my profile
-  Route::get('/edit','EventController@edit');
-  Route::put('/update', 'EventController@update');
+  Route::get('/','ProfileController@index')->name('index'); // my profile
+  Route::get('/edit','EventController@edit')->name('edit');
+  Route::put('/update', 'EventController@update')->name('update');
 });
 
 /* Comments */
 Route::middleware('auth')->prefix('comments')->name('comments.')->group(function(){
-  Route::put('/create', 'CommentController@create');
-  Route::put('/update', 'CommentController@update');
-  Route::delete('/delete', 'CommentController@destroy');
+  Route::put('/create', 'CommentController@create')->name('create');
+  Route::put('/update', 'CommentController@update')->name('update');
+  Route::delete('/delete', 'CommentController@destroy')->name('destroy');
 });
