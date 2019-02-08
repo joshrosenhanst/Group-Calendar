28938fd70dd964e23aeeb0175cff1873b34f3c2c
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-07 21:25:20 -0500

    update login element classes

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,38 +3,38 @@
 @section('title', 'Login')
 
 @section('content')
-<form id="login_form" method="POST" action="{{ route('authenticate') }}">
+<form id="login_form" class="form" method="POST" action="{{ route('authenticate') }}">
   @csrf
 
-  <header>
+  <header class="form_header">
     <h1>Group Calendar Login</h1>
   </header>
   <main>
-      <label>
-        Email Address
-      <input type="email" placeholder="Email Address" name="email" value="{{ old('email') }}">
-        @if ($errors->has('email'))
-          <span class="form-error" role="alert">
-            <strong>{{ $errors->first('email') }}</strong>
-          </span>
-        @endif
-      </label>
-      <label>
-        Password
-        <input type="password" placeholder="Password" name="password">
-        @if ($errors->has('password'))
-          <span class="form-error" role="alert">
-            <strong>{{ $errors->first('password') }}</strong>
-          </span>
-        @endif
-      </label>
-      <label class="checkbox">
-        <input type="checkbox" name="remember"> Remember Me
-      </label>
+    <label class="form_label">
+      Email Address
+    <input class="form_input" type="email" placeholder="Email Address" name="email" value="{{ old('email') }}">
+      @if ($errors->has('email'))
+        <span class="form-error" role="alert">
+          <strong>{{ $errors->first('email') }}</strong>
+        </span>
+      @endif
+    </label>
+    <label class="form_label">
+      Password
+      <input class="form_input" type="password" placeholder="Password" name="password">
+      @if ($errors->has('password'))
+        <span class="form-error" role="alert">
+          <strong>{{ $errors->first('password') }}</strong>
+        </span>
+      @endif
+    </label>
+    <label class="form_label form_checkbox">
+      <input type="checkbox" name="remember"> Remember Me
+    </label>
   </main>
   <footer>
-    <button type="submit" class="is-link">Login</button>
-    <a href="#" class="button is-text">Forgot Your Password?</a>
+    <button type="submit" class="button button-link">Login</button>
+    <a href="#" class="button button-text">Forgot Your Password?</a>
   </footer>
 </form>
 @endsection
