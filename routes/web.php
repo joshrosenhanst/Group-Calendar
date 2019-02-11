6c98a3be85a3a2583ba1ec103155f880502db985
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-11 14:30:07 -0500

    update routes

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,10 @@ Route::get('/login', 'LoginController@login')->name('login')->middleware('guest'
 Route::post('/authenticate', 'LoginController@authenticate')->name('authenticate');
 Route::get('/logout', 'LoginController@logout')->name('logout');
 
-/* Calendar */
-Route::get('/calendar','EventController@calendar')->name('calendar');
 
 /* Events */
 Route::middleware('auth')->prefix('events')->name('events.')->group(function(){
-  Route::get('/','EventController@index');
+  Route::get('/','EventController@index')->name('index');
   Route::get('/new','EventController@new')->name('new');
   Route::get('/{event}','EventController@view')->name('view');
   Route::get('/{event}/edit','EventController@edit')->name('edit');
@@ -20,6 +18,9 @@ Route::middleware('auth')->prefix('events')->name('events.')->group(function(){
   Route::put('/create', 'EventController@create')->name('create');
   Route::put('/update', 'EventController@update')->name('update');
   Route::delete('/delete', 'EventController@destroy')->name('destroy');
+
+  /* Calendar */
+  Route::get('/calendar','EventController@calendar')->name('calendar');
 });
 
 /* Groups */
@@ -27,6 +28,8 @@ Route::middleware('auth')->prefix('groups')->name('groups.')->group(function(){
   Route::get('/', 'GroupController@index')->name('index'); // my groups
   Route::get('/{group}', 'GroupController@view')->name('view'); // view group
   Route::get('/new','GroupController@new')->name('new');
+  Route::get('/{group}/events','GroupController@events')->name('events');
+  Route::get('/{group}/members','GroupController@members')->name('members');
   Route::get('/{group}/edit','GroupController@edit')->name('edit');
   Route::get('/{group}/delete','GroupController@delete')->name('delete');
   Route::get('/{group}/invite','GroupController@invite')->name('invite');
@@ -43,6 +46,11 @@ Route::middleware('auth')->prefix('profile')->name('profile.')->group(function()
   Route::get('/','ProfileController@index')->name('index'); // my profile
   Route::get('/edit','EventController@edit')->name('edit');
   Route::put('/update', 'EventController@update')->name('update');
+});
+
+/* Notifications */
+Route::middleware('auth')->prefix('notifications')->name('notifications.')->group(function(){
+  Route::get('/','NotificationController@index')->name('index');
 });
 
 /* Comments */
