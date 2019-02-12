66876317eb5d9b01f77fc2242a5bd847ec9cdcc4
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-11 21:26:12 -0500

    fix member_count text

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,5 +1,5 @@
 <?php
 return [
   /* MEMBERS */
-  'member_count' => '{0} No Members|{1} :count Member| [2*] :count Members'
+  'member_count' => '{0} No Members|{1} :count Member|[2,*] :count Members'
 ];
