2c32ca96198143efa10fbfd335e59d769857f1a1
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-12 13:29:37 -0500

    add initial maincontent_header template and styles

--- resources/views/groups/home.blade.php
+++ resources/views/groups/home.blade.php
@@ -3,9 +3,17 @@
 @section('title', $group->name)
 
 @section('content')
-<article id="maincontent" class="card">
-  <header>
-    <img src="{{ asset($group->avatar) }}" alt="{{ $group->name }} Avatar">
+<article id="maincontent">
+  <header class="maincontent_header card" style="background-image: url({{ asset($group->avatar) }})">
+    <div class="maincontent_avatar_container">
+      <a href="{{ route('groups.view', ['group'=>$group]) }}" class="maincontent_header_avatar">
+        <img src="{{ asset($group->avatar) }}" alt="{{ $group->name }} Avatar">
+      </a>
+      <div class="maincontent_header_details">
+        <a href="{{ route('groups.view', ['group'=>$group]) }}" class="maincontent_header_name">{{ $group->name }}</a>
+        <div class="maincontent_header_subtext">{{ $group->create_date }}</div>
+      </div>
+    </div>
   </header>
   <section>
     <h2>Upcoming Events</h2>
