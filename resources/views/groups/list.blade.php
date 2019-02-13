2631abc62a5e61174a940f36901df8d6540c9b38
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-13 15:19:26 -0500

    add initial design of the groups.home page

--- resources/views/groups/list.blade.php
+++ resources/views/groups/list.blade.php
@@ -3,5 +3,9 @@
 @section('title', 'My Groups')
 
 @section('content')
+<article id="maincontent">
   @each('groups.summary', $groups, 'group')
+</article>
+
+@include('layouts.sidebar')
 @endsection
--- resources/views/groups/summary.blade.php
+++ resources/views/groups/summary.blade.php
@@ -1,6 +1,22 @@
-<a href="{{ route('groups.view', [$group]) }}" class="group-summary">
-  <aside>
-    <img src="{{ asset($group->avatar) }}" alt="{{ $group->name }} Avatar">
-  </aside>
-  <h2>{{ $group->name }}</h2>
-</a>
+<div class="group-summary card card_has_avatar">
+  <div class="card_section card_section-avatar">
+    <a href="{{ route('groups.view', [$group]) }}" class="card_avatar_image">
+      <img src="{{ asset($group->avatar) }}" alt="{{ $group->name }} Avatar">
+    </a>
+    <a href="{{ route('groups.view', ['group'=>$group]) }}" class="card_avatar_name">{{ $group->name }}</a>
+
+    <div class="card_avatar_members">
+      @if($group->users_count)
+        <div class="members_list">
+          @each('members.preview_thumbnail', $group->users->take(8), 'user')
+        </div>
+      @else
+        <span class="empty">No Members</span>
+      @endif
+      <div class="card_countline">
+        <span class="countline_display">{{ trans_choice('messages.member_count',$group->users_count) }}</span>
+        <a class="countline_all_link" href="{{ route('groups.members', ['group'=>$group]) }}" class="view_all">view all</a>
+      </div>
+    </div>
+  </div>
+</div>
