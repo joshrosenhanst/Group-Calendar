c65b3c86bf866ea58eb9e5e3896cf02842b2de7e
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-14 13:23:31 -0500

    add member count to group header

--- resources/views/groups/home.blade.php
+++ resources/views/groups/home.blade.php
@@ -11,7 +11,7 @@
       </a>
       <div class="group_details">
           <a href="{{ route('groups.view', ['group'=>$group]) }}" class="group_name">{{ $group->name }}</a>
-          <div class="group_subtext"><strong>{{ trans_choice('messages.member_count',$group->users_count) }}</strong> | Created {{ $group->create_date }}</div>
+          <div class="group_subtext"><strong>{{ trans_choice('messages.member_count',$group->users_count) }}</strong> |  Created {{ $group->create_date }}</div>
       </div>
       <div class="group_links button_group button_group-inverted button_group-link button_group-small">
         <a href="{{ route('groups.events.new', ['group'=>$group]) }}" class="button">
