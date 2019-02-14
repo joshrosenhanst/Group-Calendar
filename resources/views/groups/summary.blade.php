8c5167f13de94485f5c0f5de3cba744025b7b6d4
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-14 12:59:51 -0500

    update the look of the group selection elements

--- resources/views/groups/summary.blade.php
+++ resources/views/groups/summary.blade.php
@@ -4,16 +4,16 @@
       <img src="{{ asset($group->avatar) }}" alt="{{ $group->name }} Avatar">
     </a>
     <div class="group_details">
-      <a href="{{ route('groups.view', ['group'=>$group]) }}" class="group_name">{{ $group->name }}</a>
-      <div class="group_members_preview">
-        @if($group->users_count)
-          @each('members.preview_thumbnail', $group->users->take(12), 'user')
-        @endif
-      </div>
-      <div class="group_subtext">{{ trans_choice('messages.member_count',$group->users_count) }} | Created {{ $group->create_date }}</div>
+      <a href="{{ route('groups.view', ['group'=>$group]) }}" class="group_name">
+        {{ $group->name }}
+      </a>
+      <div class="group_subtext"><strong>{{ trans_choice('messages.member_count',$group->users_count) }}</strong> | Created {{ $group->create_date }}</div>
     </div>
     <div class="group_links">
-      <a href="{{ route('groups.view', ['group'=>$group]) }}" class="button button-link button-inverted">View Group</a>
+      <a href="{{ route('groups.view', ['group'=>$group]) }}" class="button button-link button-inverted">
+        <span class="icon">@materialicon('account-group')</span>
+        <span>View Group</span>
+      </a>
     </div>
   </div>
 </div>
