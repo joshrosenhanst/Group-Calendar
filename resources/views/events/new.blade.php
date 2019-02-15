568a54a0991f0cad65a16a012f302b80821aa71b
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-15 15:28:49 -0500

    create initial new event form

--- resources/views/events/new.blade.php
+++ resources/views/events/new.blade.php
@@ -4,12 +4,96 @@
 
 @section('content')
 <article id="maincontent">
-  {{ Breadcrumbs::render('events.new') }}
-  <form action="{{ route('events.create') }}" id="event_form" class="form card">
+  <div class="card">
+    <div class="card_header card_header-no_content"></div>
+    <div class="card_section card_section-title">
+      {{ Breadcrumbs::render('events.new') }}
+      <h1 class="title">New Event</h1>
+    </div>
+    <form action="{{ route('events.create') }}" id="event_form" class="form card_section card_section-form">
       @method('PUT')
       @csrf
-      New form
-  </form>
+      
+      @include('partials.form_inline_group', [
+        'label' => ['text' => 'Event Name'],
+        'input' => [
+          'name' => 'name',
+          'type' => 'text',
+          'id' => 'name',
+          'placeholder' => 'Event Name',
+          'required' => true,
+          'old' => old('name')
+        ],
+        'help' => 'This is a help block',
+        'errors' => $errors->get('name')
+      ])
+      @include('partials.form_inline_group', [
+        'label' => ['text' => 'Group'],
+        'input' => [
+          'name' => 'group',
+          'type' => 'select',
+          'id' => 'group',
+          'placeholder' => 'Group',
+          'required' => true,
+          'default_option' => 'Select a Group',
+          'options' => Auth::user()->group_select,
+          'selected' => 1,
+          'old' => old('group')
+        ],
+        'help' => 'This is a help block',
+        'errors' => $errors->get('name')
+      ])
+      @include('partials.form_inline_group', [
+        'label' => ['text' => 'Start Date'],
+        'input' => [
+          'name' => 'start_date',
+          'type' => 'date',
+          'value' => \Carbon\Carbon::today()->toDateString(),
+          'min' => '2019-01-01',
+          'id' => 'start_date',
+          'required' => true,
+          'old' => old('start_date')
+        ],
+        'icon' => [
+          'align' => 'left',
+          'name' => 'calendar'
+        ],
+        'errors' => $errors->get('start_date')
+      ])
+      @include('partials.form_inline_group', [
+        'label' => ['text' => 'Start Time'],
+        'input' => [
+          'name' => 'start_time',
+          'type' => 'time',
+          'id' => 'start_time',
+          'old' => old('start_time')
+        ],
+        'icon' => [
+          'align' => 'left',
+          'name' => 'clock-outline'
+        ],
+        'errors' => $errors->get('start_time')
+      ])
+      @include('partials.form_inline_group', [
+        'label' => ['text' => 'Description'],
+        'input' => [
+          'name' => 'description',
+          'type' => 'textarea',
+          'id' => 'description',
+          'placeholder' => 'Description',
+          'old' => old('description')
+        ],
+        'errors' => $errors->get('description')
+      ])
+
+      <div class="form_footer">
+        <button type="submit" class="button button-link">
+          <span class="icon">@materialicon('calendar-check')</span>
+          <span>Submit Event</span>
+        </button>
+      </div>
+    </form>
+  </div>
 </article>
 @include('layouts.sidebar')
 @endsection
