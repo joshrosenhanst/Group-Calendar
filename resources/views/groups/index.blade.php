efb0db44f0541a51754c147d10187be194949708
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-15 14:00:32 -0500

    add title to groups.index

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -4,8 +4,14 @@
 
 @section('content')
 <article id="maincontent">
-  {{ Breadcrumbs::render('groups.index') }}
-  @each('components.group.summary', $groups, 'group')
+  <div class="card">
+    <div class="card_header card_header-no_content"></div>
+    <div class="card_section card_section-title">
+      {{ Breadcrumbs::render('groups.index') }}
+      <h1 class="title">My Groups</h1>
+    </div>
+    @each('components.group.summary',$groups,'group')
+  </div>
 </article>
 
 @include('layouts.sidebar')
