4a1da75d2f9198c4fc077ba7bbf9e3f9fc565e29
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-16 17:40:23 -0500

    remove unnecessary .card_body wrapper

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,52 +9,50 @@
   <header class="card_header">
     <h1>Group Calendar Login</h1>
   </header>
-  <main class="card_body">
-    <section class="card_section">
-      @include('partials.form_group', [
-        'input' => [
-          'name' => 'email',
-          'type' => 'email',
-          'id' => 'email',
-          'placeholder' => 'Email Address',
-          'aria-label' => 'Email Address',
-          'old' => old('email')
-        ],
-        'icon' => [
-          'align' => 'left',
-          'name' => 'email'
-        ],
-        'errors' => $errors->get('email')
-      ])
-      @include('partials.form_group', [
-        'input' => [
-          'name' => 'password',
-          'type' => 'password',
-          'id' => 'password',
-          'placeholder' => 'Password',
-          'aria-label' => 'Email Address',
-        ],
-        'icon' => [
-          'align' => 'left',
-          'name' => 'lock'
-        ],
-        'errors' => $errors->get('password')
-      ])
-      @include('partials.form_group_checkbox', [
-        'label' => ['text'=>'Remember Me'],
-        'input' => [
-          'name' => 'remember',
-          'type' => 'checkbox',
-          'value' => 1,
-          'old' => old('remember')
-        ],
-        'errors' => $errors->get('remember')
-      ])
-      <div class="button_group">
-        <button type="submit" class="button button-link">Login</button>
-        <a href="#">Forgot Your Password?</a>
-      </div>
-    </section>
-  </main>
+  <section class="card_section">
+    @include('partials.form_group', [
+      'input' => [
+        'name' => 'email',
+        'type' => 'email',
+        'id' => 'email',
+        'placeholder' => 'Email Address',
+        'aria-label' => 'Email Address',
+        'old' => old('email')
+      ],
+      'icon' => [
+        'align' => 'left',
+        'name' => 'email'
+      ],
+      'errors' => $errors->get('email')
+    ])
+    @include('partials.form_group', [
+      'input' => [
+        'name' => 'password',
+        'type' => 'password',
+        'id' => 'password',
+        'placeholder' => 'Password',
+        'aria-label' => 'Email Address',
+      ],
+      'icon' => [
+        'align' => 'left',
+        'name' => 'lock'
+      ],
+      'errors' => $errors->get('password')
+    ])
+    @include('partials.form_group_checkbox', [
+      'label' => ['text'=>'Remember Me'],
+      'input' => [
+        'name' => 'remember',
+        'type' => 'checkbox',
+        'value' => 1,
+        'old' => old('remember')
+      ],
+      'errors' => $errors->get('remember')
+    ])
+    <div class="button_group">
+      <button type="submit" class="button button-link">Login</button>
+      <a href="#">Forgot Your Password?</a>
+    </div>
+  </section>
 </form>
 @endsection
