53370ce3f4b394b7d46b741b1c2d261f005248fc
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-16 20:37:41 -0500

    fix route() issue in event_summary template

--- resources/views/blocks/events/summary.blade.php
+++ resources/views/blocks/events/summary.blade.php
@@ -1,5 +1,5 @@
 <div class="event_summary card">
-  <a class="summary_header" href="{{ route('events.view', ['event', $event]) }}">
+  <a class="summary_header" href="{{ route('events.view', ['event'=>$event]) }}">
     <img src="{{ asset($event->header) }}" alt="{{ $event->name }} header image">
   </a>
   <div class="summary_details card_section">
@@ -9,11 +9,11 @@
       <span class="month">{{ $event->start_date->format('M') }}</span>
     </div>
     <div class="summary_info">
-      <a class="summary_name" href="{{ route('events.view', ['event', $event]) }}">{{ $event->name }}</a>
+      <a class="summary_name" href="{{ route('events.view', ['event'=>$event]) }}">{{ $event->name }}</a>
       <div class="summary_location">Brooklyn, NY</div>
     </div>
   </div>
   <div class="card_section card_buttons">
-    <a href="{{ route('events.view', ['event', $event]) }}" class="button button-text button-small">View Event</a>
+    <a href="{{ route('events.view', ['event'=>$event]) }}" class="button button-text button-small">View Event</a>
   </div>
 </div>
