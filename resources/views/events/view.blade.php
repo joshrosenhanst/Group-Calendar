5119b7e8e3d9e7be874ab033feb47dba18023418
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-18 15:28:14 -0500

    initial redesign of the event.view template

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -4,11 +4,30 @@
 
 @section('content')
 <article id="maincontent">
-  <div class="card">
+  <div class="card event_card">
     <div class="card_header card_header-no_content"></div>
     <div class="card_section card_section-title">
       {{ Breadcrumbs::render('events.view', $event) }}
-      <h1 class="title">{{ $event->name }}</h1>
+    </div>
+    <div class="card_section card_section-background_image" style="background-image:url({{ asset($event->header) }})"></div>
+    <div class="card_section event_card_section-main">
+      <h1 class="event_title">{{ $event->name }}</h1>
+      <div class="event_location">
+        <a href="#location" class="button button-text">
+          <span class="icon">@materialicon('map-marker')</span>
+          <span>Brooklyn, NY</span>
+        </a>
+      </div>
+      <div class="attend_buttons button_group">
+        <a href="#" class="button button-link button-inverted">
+          <span class="icon">@materialicon('check')</span>
+          <span>Going</span>
+        </a>
+        <a href="#" class="button button-info button-inverted">
+          <span class="icon">@materialicon('star')</span>
+          <span>Interested</span>
+        </a>
+      </div>
     </div>
     <div class="card_section">
       <dl>
