15d36f65a0e6da22b4329bfc780c842c0cea664b
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-18 20:40:53 -0500

    update the look and layout of the .event_card

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -13,18 +13,22 @@
     <div class="card_section event_card_section-main">
       <h1 class="event_title">{{ $event->name }}</h1>
       <div class="event_date event_detail">
-        <div class="detail_icon">
-          <span class="icon">@materialicon('calendar')</span>
-        </div>
+        <span class="icon icon-full_size">@materialicon('calendar')</span>
         <div class="detail_content">
           {{ $event->summary_date }}
         </div>
       </div>
-      <div class="event_location">
-        <a href="#location" class="button button-text">
-          <span class="icon">@materialicon('map-marker')</span>
-          <span>Brooklyn, NY</span>
-        </a>
+      <div class="event_detail">
+        <div class="icon icon-full_size">@materialicon('map-marker')</div>
+        <div class="detail_content">
+          <a href="#">1256 Franklin St<small>Brooklyn, NY 07747</small></a>
+        </div>
+      </div>
+      <div class="event_detail">
+        <div class="icon icon-full_size">@materialicon('account-group')</div>
+        <div class="detail_content">
+          <a href="#attending">{{ $event->group->name }}<small>3 Going | 4 Interested</small></a>
+        </div>
       </div>
       <div class="attend_buttons button_group">
         <a href="#" class="button button-link button-inverted">
@@ -37,34 +41,23 @@
         </a>
       </div>
     </div>
-    <div class="card_section">
-      <dl>
-        <dt>Name</dt>
-        <dd>{{ $event->name }}</dd>
-        <dt>Group</dt>
-        <dd><a href="{{ route('groups.view', ['group'=>$event->group]) }}">{{ $event->group->name }}</a></dd>
-        <dt>Created By</dt>
-        <dd>
-          <a href="{{ route('users.view', ['user'=>$event->creator]) }}">{{ $event->creator->name }}</a>
-        </dd>
-        <dt>Description</dt>
-        <dd>{{ $event->description }}</dd>
-        <dt>Start Date</dt>
-        <dd>
-          {{ $event->start_date->format('l, F j, Y') }}
-          @isset($event->start_time_subtext)
-            <small>{{ $event->start_time_subtext }}</small>
-          @endisset
-        </dd>
-        @isset($event->end_date)
-          <dt>End Date</dt>
+    <div class="card_section event_card_section-description">
+      <div class="description">
+        {{ $event->description }}
+      </div>
+      <dl class="event_description_list">
+        <div class="description_list_group">
+          <dt>Created By</dt>
           <dd>
-            {{ $event->end_date->format('l, F j, Y') }}
-            @isset($event->end_time_subtext)
-              <small>{{ $event->end_time_subtext }}</small>
-            @endisset
+            <a href="{{ route('users.view', ['user'=>$event->creator]) }}">{{ $event->creator->name }}</a>
           </dd>
-        @endisset
+        </div>
+        <div class="description_list_group">
+          <dt>Event Link</dt>
+          <dd>
+            <a href="{{ route('events.view', ['event'=>$event]) }}">{{ route('events.view', ['event'=>$event]) }}</a>
+          </dd>
+        </div>
       </dl>
     </div>
   </div>
