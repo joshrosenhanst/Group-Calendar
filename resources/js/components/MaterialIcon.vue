b17e559bfdc6dc2bb7890bb38d551c962aeadfd6
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-19 17:59:45 -0500

    add MaterialIcon vue component for inserting SVG icons into vue templates

---
+++ resources/js/components/MaterialIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="icon">
+    <svg 
+      viewBox="0 0 24 24" 
+      xmlns="http://www.w3.org/2000/svg" 
+      v-bind:width="size"
+      v-bind:height="size" 
+      role="presentation"
+    >
+      <path v-bind:d="icons[name]"></path>
+    </svg>
+  </span>
+</template>
+
+<script>
+import Icons from '../icons.js';
+export default {
+  data: function() {
+    return {
+      icons: Icons
+    };
+  },
+  props: {
+    size: {
+      type: Number,
+      default: 24
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
