51678e9d750f9278d5d15a47bcfe117b01902b08
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-18 20:49:24 -0500

    remove helper text from blocks.comments.empty

--- resources/views/blocks/comments/empty.blade.php
+++ resources/views/blocks/comments/empty.blade.php
@@ -1,5 +1,4 @@
 <div class="comments-empty">
   <span class="icon">@materialicon('comment-question-outline')</span>
   <h2>No Comments Found</h2>
-  <div class="sub_text">You can leave a comment on the group or on a group event.</div>
 </div>
