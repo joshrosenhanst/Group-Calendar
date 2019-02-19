8e0aced7d3151eb0d269ddf4ab83719883dfaeda
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-19 13:38:09 -0500

    add status.php language file for attendee statuses

---
+++ resources/lang/en/status.php
@@ -0,0 +1,13 @@
+<?php
+return [
+  /* Attendee Status */
+  'attendee.pending' => 'Pending',
+  'attendee.interested' => 'Interested',
+  'attendee.going' => 'Going',
+  'attendee.unavailable' => 'Unavailable',
+  /* Attendee Status Icons */
+  'attendee.icon.pending' => 'account-question',
+  'attendee.icon.interested' => 'star',
+  'attendee.icon.going' => 'account-check',
+  'attendee.icon.unavailable' => 'account-remove',
+];
