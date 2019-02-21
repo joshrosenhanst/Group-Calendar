43b4c11116b1d1889fe0f6ea0f6c55af5e6502c6
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-21 14:59:24 -0500

    create event comment ajax controller functionality

--- app/Http/Controllers/Ajax/EventController.php
+++ app/Http/Controllers/Ajax/EventController.php
@@ -14,7 +14,7 @@ class EventController extends Controller
   public function attend(Request $request, \App\Event $event){
     $request->validate([
       'status'=>'required|in:pending,interested,going,unavailable',
-      'user_id'=>'required|numeric'
+      'user_id'=>'required|numeric|exists:users,id'
     ]);
 
     $new_status = $request->input('status', 'pending');
@@ -32,5 +32,52 @@ class EventController extends Controller
     }
     //trigger "event attendee updated" Event
     return response()->json($new_status);
+  }
+
+  /*
+    createComment() - Create an event comment. Return the event comments.
+  */
+  public function createComment(Request $request, \App\Event $event){
+    $request->validate([
+      'text'=>'required',
+      'user_id'=>'required|numeric|exists:users,id'
+    ]);
+
+    $comment = $event->comments()->create([
+      'text' => $request->text,
+      'user_id' => $request->user_id
+    ]);
+    //trigger "comment created" Event
+    $event->loadMissing('comments.user');
+    return response()->json($event->comments);
+  }
+
+  /*
+    updateComment() - Update an event comment. Return the event comments.
+  */
+  public function updateComment(Request $request, \App\Event $event, \App\Comment $comment){
+    $request->validate([
+      'text'=>'required'
+    ]);
+
+    $comment->update([
+      'text' => $request->text
+    ]);
+
+    //trigger "comment updated" Event
+    $event->loadMissing('comments.user');
+    return response()->json($event->comments);
+  }
+  
+  /*
+    destroyComment() - Delete an event comment. Return the event comments.
+  */
+  public function destroyComment(\App\Event $event, \App\Comment $comment){
+
+    $comment->delete();
+
+    //trigger "comment deleted" Event
+    $event->loadMissing('comments.user');
+    return response()->json($event->comments);
   }
 }
