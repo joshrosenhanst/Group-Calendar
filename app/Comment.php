9a11f2b563b6ba1f4fc563389b75e42edc8320d0
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-21 14:55:27 -0500

    set comment text & user_id to be fillable on the model

--- app/Comment.php
+++ app/Comment.php
@@ -13,6 +13,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+  protected $fillable = [
+    'text', 'user_id'
+  ];
+  
   public function commentable(){
     return $this->morphTo();
   }
