84b85f107a15429d4c4939833882bc1933e077e0
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-22 18:20:10 -0500

    replace @each directive with @foreach to better manage the component slots

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -5,12 +5,26 @@
 @section('content')
 <article id="maincontent">
   <div class="card">
+    {{-- Page Header --}}
     <div class="card_header card_header-no_content"></div>
     <div class="card_section card_section-title">
       {{ Breadcrumbs::render('groups.index') }}
       <h1 class="title">My Groups</h1>
     </div>
-    @each('components.group.summary',$groups,'group')
+
+    {{-- List of Groups --}}
+    @foreach($groups as $group)
+      @component('components.group.summary', ['group'=>$group])
+        @slot('links')
+        <div class="group_links">
+          <a href="{{ route('groups.view', ['group'=>$group]) }}" class="button button-link button-inverted">
+            <span class="icon">@materialicon('account-group')</span>
+            <span>View Group</span>
+          </a>
+        </div>
+        @endslot
+      @endcomponent
+    @endforeach
   </div>
 </article>
 
