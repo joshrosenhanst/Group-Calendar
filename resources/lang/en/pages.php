3ca0c7cf70544cf3106c7807bb65b7ada8dacfbd
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-23 14:35:04 -0500

    create a language file for page titles/subtitles

---
+++ resources/lang/en/pages.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'groups.index.title' => 'My Groups',
+  'groups.index.subtitle' => 'Choose a group from your list or create a new group. You can invite members to your group and create events in the group.',
+];
