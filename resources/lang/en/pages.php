544e08c8f2f56d68b2c980c76c5f4c82af9e670e
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-23 16:41:22 -0500

    add page titles/subtitles

--- resources/lang/en/pages.php
+++ resources/lang/en/pages.php
@@ -1,6 +1,18 @@
 <?php
 
 return [
+  /* Groups */
   'groups.index.title' => 'My Groups',
-  'groups.index.subtitle' => 'Choose a group from your list or create a new group. You can invite members to your group and create events in the group.',
+  'groups.index.subtitle' => 'Choose a group from your list or create a new group. You can invite members to your group and create events for the group. Group events are private and only group members can attend.',
+  'groups.view.title' => 'Group Details',
+  'groups.members.title' => 'Group Members',
+  'groups.members.subtitle' => 'The list of active members for this group. You can invite new members to join the group.',
+  'groups.members.subtitle.admin' => "The list of active members for this group. You can invite new members to join the group. Admins can also change a member's status or remove a member.",
+
+  /* Events */
+  'events.new.title' => 'New Event',
+  'events.new.subtitle' => "Enter the details for a new group event below. The event will appear on the group calendar and only this group's members can attend or comment. You can edit the details after you've submitted the event.",
+  'events.edit.title' => 'Edit Event',
+  'events.edit.subtitle' => "Edit the details of the event. You can optionally leave a comment about your changes.",
+  'events.view.title' => 'Event Details',
 ];
