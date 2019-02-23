26dae60643e21e5023177e1a6ab4536b53c7f7e1
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-23 14:19:34 -0500

    create new sidebar for the groups.index page

---
+++ resources/views/layouts/sidebar/groups.blade.php
@@ -0,0 +1,19 @@
+<div class="sidebar">
+  <div class="sidebar_header sidebar_header-no_content"></div>
+  <div class="sidebar_header">
+    <h2 class="title">
+      <span class="icon">@materialicon('account-group')</span>
+      <span>My Groups</span>
+    </h2>
+    <div class="subtitle">
+      Choose a group from your list or create a new group. You can invite members to your group and manage or create events in the group.
+    </div>
+  </div>
+  <div class="sidebar_section sidebar_links">
+    <a href="{{ route('groups.new') }}" class="sidebar_link">
+      <span class="icon">@materialicon('plus-circle')</span>
+      <span>Create New Group</span>
+    </a>
+  </div>
+  <div class="sidebar_footer sidebar_footer-no_content"></div>
+</div>
