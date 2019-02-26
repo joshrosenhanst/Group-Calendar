0f016940171f275a806ab2c20d95d33c0841dab1
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-26 15:06:12 -0500

    update the look of the upcoming_events block, add link

--- resources/views/blocks/groups/upcoming_events.blade.php
+++ resources/views/blocks/groups/upcoming_events.blade.php
@@ -1,8 +1,12 @@
 <div class="card">
   <div class="card_header">
     <h2>
+      <span>Upcoming Events</span>
+    </h2>
+    <a href="{{ route('groups.events', ['group'=>$group]) }}" class="button">
       <span class="icon">@materialicon('calendar-range')</span>
-      <span>Upcoming Events</span></h2>
+      <span>View All Events</span>
+    </a>
   </div>
   <div class="card_section upcoming_events_section">
     @each('blocks.events.summary', $events, 'event', 'blocks.events.empty')
