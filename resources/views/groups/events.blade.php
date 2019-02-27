961783c03dd0b7fc73d0d0509b74d1290b6b075f
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-27 18:30:19 -0500

    update the template/styles of group.events to use upcoming events by month

--- resources/views/groups/events.blade.php
+++ resources/views/groups/events.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.landing')
 
-@section('title', 'Calendar')
+@section('title', 'Group Events')
 
 @section('content')
 <article id="maincontent">
@@ -30,41 +30,43 @@
     </ul>
   </div>
 
-  <div class="card card-has_tabs">
-  <div class="card_sub_header">
-    <h1 class="sub_title">Events from 2018</h1>
-  </div>
-  <div class="card_sub_header">
-    <h2 class="sub_title">February Events</h2>
-  </div>
-  @foreach($group->events as $event)
-    <div class="event_summary">
-      <div class="summary_header summary_header-background_image" style="background-image: url({{ asset($event->header) }})">&nbsp;</div>
-      <div class="card_section summary_details">
-        <div class="summary_date">
-          <span class="day">{{ $event->start_date->format('D') }}</span>
-          <span class="date">{{ $event->start_date->format('d') }}</span>
-          <span class="month">{{ $event->start_date->format('M') }}</span>
-        </div>
-        <div class="summary_info">
-          <a class="summary_name" href="{{ route('events.view', ['event'=>$event]) }}">{{ $event->name }}</a>
-          <div class="summary_location">Brooklyn, NY</div>
-          <div class="summary_status">
-            <span class="icon icon-full_size">@materialicon('account-question')</span>
-            <strong class="status">Pending</strong> · <a href="{{ route('events.view', ['event'=>$event]) }}">Update My Status</a>
+    
+  @foreach($monthly_upcoming_events as $month=>$monthly_events)
+    <div class="card card-has_tabs">
+      <div class="card_sub_header">
+        <h1 class="sub_title">{{$month}} Events</h1>
+      </div>
+
+      @foreach($monthly_events as $event)
+      <div class="event_summary">
+        <div class="summary_header summary_header-background_image" style="background-image: url({{ asset($event->header) }})">&nbsp;</div>
+        <div class="card_section summary_details">
+          <div class="summary_date">
+            <span class="day">{{ $event->start_date->format('D') }}</span>
+            <span class="date">{{ $event->start_date->format('d') }}</span>
+            <span class="month">{{ $event->start_date->format('M') }}</span>
+          </div>
+          <div class="summary_info">
+            <a class="summary_name" href="{{ route('events.view', ['event'=>$event]) }}">{{ $event->name }}</a>
+            <div class="summary_location"><span>{{ $event->start_time_subtext }}</span> · <span>Brooklyn, NY</span></div>
+            <div class="summary_status">
+              <span class="status_display">
+                <span class="icon icon-full_size">@materialicon('account-question')</span><strong class="status">Pending</strong>
+              </span> · <a href="{{ route('events.view', ['event'=>$event]) }}" class="update_status">Update My Status</a>
+            </div>
+          </div>
+          <div class="summary_links">
+            <a href="{{ route('events.view', ['event'=>$event]) }}" class="button button-link button-inverted">
+              <span class="icon">@materialicon('calendar-text-outline')</span>
+              <span>Details</span>
+            </a>
           </div>
         </div>
-        <div class="summary_links">
-          <a href="{{ route('events.view', ['event'=>$event]) }}" class="button button-link button-inverted">
-            <span class="icon">@materialicon('calendar-text-outline')</span>
-            <span>Details</span>
-          </a>
-        </div>
       </div>
+      @endforeach
     </div>
-    @endforeach
+  @endforeach
 
-  </div>
 </article>
 
 {{-- Sidebars --}}
