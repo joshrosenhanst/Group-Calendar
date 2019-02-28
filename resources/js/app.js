0b60db6445ac2741153ea8b20bca5dac162643ba
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-02-27 19:06:27 -0500

    create TabWrapper vue component for tab switching

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.component('tab-wrapper', require('./components/tabs/TabWrapper.vue').default);
 Vue.component('attendee-status', require('./components/AttendeeStatus.vue').default);
 Vue.component('material-icon', require('./components/MaterialIcon.vue').default);
 Vue.component('comments-card', require('./components/CommentsCard.vue').default);
---
+++ resources/js/components/tabs/TabWrapper.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="tabs">
+    <ul>
+      <li
+        v-for="(tabItem, index) in tabItems"
+        v-bind:key="tabItem"
+        v-bind:class="{ 'tab_active':(activeTab === index) }"
+      >
+        <a v-on:click="selectTab(index)"
+        >
+          <slot v-bind:name="tabItem"></slot>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    tabItems: Array
+  },
+  data: function(){
+    return {
+      activeTab: 0
+    };
+  },
+  methods: {
+    selectTab(tabIndex){
+      if(this.activeTab === tabIndex) return;
+
+      this.activeTab = tabIndex;
+      this.$emit('select-tab', this.tabItems[tabIndex]);
+    }
+  }
+}
+</script>
