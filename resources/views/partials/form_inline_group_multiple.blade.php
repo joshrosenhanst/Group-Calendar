2b7b227b9304666a2b261bb601bf90f3ae251190
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-03-01 12:44:25 -0500

    create form_inline_group_multiple for form_groups with multiple input fields

---
+++ resources/views/partials/form_inline_group_multiple.blade.php
@@ -0,0 +1,51 @@
+<div class="{{ $has_errors?'form_inline_group form_group-error':'form_inline_group'}}">
+  <div class="field">
+    {{-- FORM LABEL --}}
+    <div class="field_label">
+      @isset($label)
+      <label
+        @isset( $label['label_for'] )
+        for="{{ $label['label_for'] }}"
+        @endisset
+        class="{{ $label['class'] ?? 'form_label' }}">
+          {{ $label['text'] }}
+          @isset($label['required'])<span class="required">*</span>@endisset
+      </label>
+      @endisset
+    </div>
+  
+    <div class="field_body">
+      <div class="field_multiple">
+
+        @foreach($inputs as $input)
+        <div class="field_input">
+          {{-- ICON SUPPORT --}}
+          @isset($input['icon'])
+            <div class="form_input_icon {{ $input['icon']['align'] == 'right'?'form_input_icon-icon_right':'form_input_icon-icon_left' }}">
+              @include('partials.input', [
+                'input' => $input, 
+                'icon'=>$input['icon'] 
+              ])
+            </div>
+          @else
+            @include('partials.input', ['input' => $input])
+          @endisset
+        </div>
+        @endforeach
+        
+      </div>
+
+      {{-- FORM HELP --}}
+      @isset($help)
+        <div class="form_help">{{ $help }}</div>
+      @endisset
+
+      {{-- FORM ERRORS --}}
+      @foreach ($error_group as $errors)
+        @foreach($errors as $error)
+        <div class="form_error" role="alert">{{ $error }}</div>
+        @endforeach
+      @endforeach
+    </div>
+  </div>
+</div>
