4210f0272e622eb5818348fda56f0fc73871402f
Josh Mint VM <josh.rosenhanst@gmail.com>
2019-03-06 10:44:55 -0500

    create the StatusAlert vue component to replace the blade template

---
+++ resources/js/components/StatusAlert.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="alert" v-if="statusVisible">
+
+    <div class="alert_buttons" v-if="closeButton">
+      <button class="button-icon" aria-label="Hide Alert" 
+        v-on:click="statusVisible = false"
+      >
+        <material-icon name="close"></material-icon>
+      </button>
+    </div>
+
+    <div class="alert_icon" v-if="icon">
+      <material-icon v-bind:name="icon"></material-icon>
+    </div>
+
+    <div class="alert_text">
+      <slot></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      statusVisible: true
+    };
+  },
+  props: {
+    icon: {
+      type: String,
+      required: false
+    },
+    closeButton: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
