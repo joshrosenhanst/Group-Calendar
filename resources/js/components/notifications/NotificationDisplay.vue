7e030de1384d5bbe0af1a8a4db5b07795ed415ca
Josh RosenHanst <josh.rosenhanst@gmail.com>
2019-03-19 18:16:54 -0400

    decuple NotificationDisplay from dropdown component to get better keyboard control

--- resources/js/components/notifications/NotificationDisplay.vue
+++ resources/js/components/notifications/NotificationDisplay.vue
@@ -1,26 +1,43 @@
 <template>
-  <app-dropdown class="navbar_dropdown"
-    aria-label="Unread Notifications dropdown" v-bind:title="title"
-    v-on:dropdown-active="markUserNotifcationsAsRead"
+  <div class="navbar_dropdown dropdown"
+    aria-haspopup="true"
+    role="button"
+    aria-label="Unread Notifications dropdown"
+    :class="{ 'dropdown-active': isActive }"
+    :aria-expanded="isActive"
+    :title="title"
   >
-    <a href="/notifications" class="navbar_item navbar_item-icon has_badge" slot="trigger">
-      <span class="badge" v-show="unread_notifications">
-        {{ unread_notifications }}
-      </span>
-      <material-icon name="bell"></material-icon>
-    </a>
+    <div class="dropdown_toggle" ref="trigger">
+      <a href="/notifications" class="navbar_item navbar_item-icon has_badge"
+        @click.prevent="openDropdown"
+        @focus="openDropdown"
+        @blur.prevent="onBlur"
+      >
+        <span class="badge" v-show="unread_notifications">
+          {{ unread_notifications }}
+        </span>
+        <material-icon name="bell"></material-icon>
+      </a>
+    </div>
 
-    <slot slot="dropdown_items"></slot>
-
-  </app-dropdown>
+    <div class="dropdown_items" 
+      aria-role="menu"
+      ref="dropdown_items"
+      v-bind:aria-hidden="!isActive"
+      @blur.capture="onBlur"
+    >
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  data: function(){
+  data(){
     return {
+      isActive: false,
       unread_notifications: (this.count || 0)
-    };
+    }
   },
   props: {
     user_id: Number,
@@ -35,6 +52,27 @@ export default {
         }).catch((error) => {
           console.log(error);
         });
+      }
+    },
+    openDropdown(){
+      this.isActive = true;
+      this.markUserNotifcationsAsRead();
+    },
+    closeDropdown(){
+      this.isActive = false;
+    },
+    /*
+      checkWhitelist() - Return whether the event.target is the $refs.trigger field or a child of the $refs.dropdown_items.
+    */
+    checkWhitelist(target){
+      return (target === this.$refs.trigger || this.$refs.trigger.contains(target) || target === this.$refs.dropdown_items || this.$refs.dropdown_items.contains(target));
+    },
+    /*
+      onBlur() - On $refs.trigger blur, check if the event.relatedTarget is either the trigger field or a child of $refs.dropdown_items. If not we can close the dropdown.
+    */
+    onBlur(event){
+      if( !this.checkWhitelist(event.relatedTarget) ) {
+        this.closeDropdown();
       }
     },
   },
