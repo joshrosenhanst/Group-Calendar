fcea300093704497110098cfe41c2c9dd2205363
Josh RosenHanst <josh.rosenhanst@gmail.com>
2019-03-21 15:26:18 -0400

    remove unused files

--- resources/views/blocks/attendees/list_item.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="attendee_list_preview list_item">
-  <a class="preview_thumbnail" href="{{ route('users.view', ['user'=>$user]) }}">
-    <img src="{{ asset($user->avatar) }}" alt="{{ $user->name }} Avatar">
-  </a>
-  <a href="{{ route('users.view', ['user'=>$user]) }}" class="preview_name">
-    {{ $user->name }} 
-    <small>{{ __('status.attendee.'.$user->pivot->status) }}</small>
-  </a>
-</div>
