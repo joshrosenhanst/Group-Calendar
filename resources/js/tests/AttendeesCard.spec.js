43322f6c55d9e3617c4815cdb8e438f72f4aa5b0
Josh RosenHanst <josh.rosenhanst@gmail.com>
2019-03-25 18:40:32 -0400

    create tests for AttendeesCard component

---
+++ resources/js/tests/AttendeesCard.spec.js
@@ -0,0 +1,38 @@
+import { shallowMount } from '@vue/test-utils';
+import AttendeesCard from '../components/attendees/AttendeesCard.vue';
+import MaterialIcon from '../components/icons/MaterialIcon.vue';
+
+const attendees = [
+  {id: 1, name: "Charles Test", avatar: "avatar_image1.png", pivot: {
+    status: 'pending'
+  }},
+  {id: 2, name: "Maxine Test", avatar: "avatar_image2.png", pivot: {
+    status: 'unavailable'
+  }}
+];
+
+describe('AttendeesCard.vue', () => {
+  it('renders list_items for each attendee in props.attendees', () => {
+
+    const wrapper = shallowMount(AttendeesCard, {
+      propsData: { attendees },
+      stubs: {
+        'material-icon': MaterialIcon
+      }
+    });
+
+    expect(wrapper.findAll('.list_item')).toHaveLength(attendees.length);
+  });
+
+  it('renders an empty display if props.attendees is empty', () => {
+
+    const wrapper = shallowMount(AttendeesCard, {
+      propsData: { 'attendees': [] },
+      stubs: {
+        'material-icon': MaterialIcon
+      }
+    });
+
+    expect(wrapper.html()).toContain('<h2>No Attendees</h2>');
+  });
+});
